36eb5e6aeb313ab7ff9ab526ac6b227c874fe56c
mariam <sargsyanmariam901@gmail.com>
2023-04-04 21:51:16 +0100

    add login functionality test

--- test/pageobjects/login.page.ts
+++ test/pageobjects/login.page.ts
@@ -2,38 +2,60 @@ import { ChainablePromiseElement } from 'webdriverio';
 
 import Page from './page.ts';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
+    /* selectors */
     public get inputUsername () {
-        return $('#username');
+        return $('[data-qa-id="email-input"]');
     }
 
     public get inputPassword () {
-        return $('#password');
+        return $('[data-qa-id="password-input"]');
     }
 
-    public get btnSubmit () {
-        return $('button[type="submit"]');
+    public get loginBtn () {
+        return $('[data-qa-id="login-btn"]');
     }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
+    public get errorDisplay () {
+        return $('[data-qa-id="error-display"]');
+    }
+
+    public get rememberMeCheckbox () {
+        return $('[data-qa-id="remember-me-checkbox-label]')
+    }
+
+    public get checkedCheckbox () {
+        return $('.uni-form__check-item--is-checked')
+    }
+
+    /* Page Methods */
+
+    public async clearLoginFields() {
+       await (await this.inputUsername).clearValue();
+       await (await this.inputPassword).clearValue();
+    }
+
     public async login (username: string, password: string) {
         await this.inputUsername.setValue(username);
         await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
+        await this.loginBtn.click();
     }
 
-    /**
-     * overwrite specific options to adapt it to page object
-     */
+    public async clickRememberMeCheckbox() {
+        const rememberMeCheckbox = await this.rememberMeCheckbox;
+        const isChecked = await (await this.checkedCheckbox).isExisting();
+    
+        if (!isChecked) {
+            await rememberMeCheckbox.click();
+            await browser.waitUntil(async () => {
+                return (await this.checkedCheckbox).isExisting();
+            }, {
+                timeout: 5000, 
+                timeoutMsg: 'Remember Me checkbox was not checked within 5 seconds'
+            });
+        }
+    }
+    
     public open () {
         return super.open('login');
     }
--- test/pageobjects/page.ts
+++ test/pageobjects/page.ts
@@ -2,12 +2,14 @@
 * main page object containing all methods, selectors and functionality
 * that is shared across all page objects
 */
+declare const browser: WebdriverIO.Browser;
+
 export default class Page {
     /**
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
     public open (path: string) {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`)
+        return browser.url(`https://www.hudl.com/${path}`)
     }
 }
