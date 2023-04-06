8dd5190f97d16190cc649df99f26be721864424f
mariam <sargsyanmariam901@gmail.com>
2023-04-06 16:07:25 +0100

    renamed file

---
+++ test/pageobjects/home.page.ts
@@ -0,0 +1,38 @@
+import { ChainablePromiseElement } from 'webdriverio';
+
+import Page from './page.js';
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class HomePage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    public get navigationPanel () {
+        return $('.hui-globalnav');
+    }
+
+    public get logOutBtn () {
+        return $('[data-qa-id="webnav-usermenu-logout"]');
+    }
+
+    public get globalUserMenu () {
+        return $('.hui-globalusermenu');
+    }
+
+    /**
+     * page methods
+     */
+
+    public open () {
+        return super.open('home');
+    }
+
+    async logOut() {
+        await (await this.globalUserMenu).click();
+        await (await this.logOutBtn).click()
+    }
+}
+
+export default new HomePage();
